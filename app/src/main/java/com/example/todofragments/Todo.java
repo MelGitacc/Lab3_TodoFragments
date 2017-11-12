2d05368787eee1d134f910b3a5c6adb4dc390f72
MelGitAcc <emgeexy@yahoo.com>
2017-11-12 12:08:18 +0000

    Initial commit

---
+++ app/src/main/java/com/example/todofragments/Todo.java
@@ -0,0 +1,65 @@
+package com.example.todofragments;
+
+/**
+ * Created by Melody on 12/11/2017.
+ */
+
+import java.util.Date;
+import java.util.UUID;
+
+
+
+public class Todo {
+
+    private UUID mId;
+    private String mTitle;
+    private String mDetail;
+    private Date mDate;
+    private boolean mIsComplete;
+
+    public Todo() {
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public void setIsComplete(boolean todoIsComplete) {
+        mIsComplete = todoIsComplete;
+    }
+
+    public boolean isIsComplete() {
+
+        return mIsComplete;
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public String getDetail() {
+        return mDetail;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setId(UUID todoId) {
+        mId = todoId;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public void setDetail(String detail) {
+        mDetail = detail;
+    }
+
+    public void setDate(Date todoDate) {
+        mDate = todoDate;
+    }
+}
